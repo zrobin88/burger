e403fa063e775f397015a2ceed675b4314f30453
Zachary Robin <zacharyrobin@zacharys-mbp-2.home>
2019-03-10 19:54:31 -0400

    all done

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Randy's Classic Cheesburger", TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bobandy's Gut Buster", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheesburger Walrus Special", TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Dirty Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Philadelphia Collins Extra Dirty Burger", TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Mustard Tiger", FALSE);
