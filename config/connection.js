4c063b53fd4cd75600ccaaafff8648771e98f3cf
Zachary Robin <zacharyrobin@Zacharys-MacBook-Pro-2.local>
2019-03-12 19:11:59 -0400

    updates

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,17 @@
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "@Stang88gt",
-    database: "burgers_db"
-  });
+
+  if(process.env.JAWSDB_URL){
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+   } else {
+    var connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "@Stang88gt",
+      database: "burgers_db"
+    });
+   }
 
  connection.connect(function(err){
     if (err){
